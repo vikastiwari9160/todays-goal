79b68956b7b5454b4e0521e0b411566a7ef2ba2f
VIKAS TIWARI <vkt9160@gmail.com>
2023-12-15 18:57:55 +0530

    added scrollbarr in the list menu

--- popup.js
+++ popup.js
@@ -1,14 +1,20 @@
 let taskBox = document.getElementById("Task-input");
 let taskList = document.getElementById("Task-list");
+let add = document.getElementById("add-Task");
+// localStorage.clear();
 restore();
-
 function addTask() {
     let task = taskBox.value;
-    let li = document.createElement("li");
-    li.innerHTML = task + "<button onclick='del()'>X</button>"; 
-    taskList.appendChild(li);
-    taskBox.value ="";
-    save();
+    if(task===''){
+        alert("Enter a Task in the input box!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = task + "<button onclick='del()'>X</button>"; 
+        taskList.appendChild(li);
+        taskBox.value ="";
+        save();
+    }
 }
 
 function del() {
